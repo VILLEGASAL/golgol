1a8425eeb13c22afba659e9d2f12711345c89bfe
unknown <villegasalrandolph@gmail.com>
2026-03-23 21:17:10 +0800

    Added authentication route

--- src/server.py
+++ src/server.py
@@ -1,17 +1,33 @@
-from fastapi import FastAPI, Form, Query, UploadFile, File, Path
-from typing import Annotated, Optional, List
+from fastapi import FastAPI, Form, Query, UploadFile, File, Path, HTTPException
+from typing import Annotated, Optional, List, Dict
 from pydantic import BaseModel
 from fastapi.requests import Request
 from fastapi.responses import RedirectResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from jose import JWTError, jwt
+from passlib.context import CryptContext 
+from dotenv import load_dotenv, dotenv_values
 import os
 import shutil
 import mimetypes
+import hashlib
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCES_TOKEN_EXPIRES_IN_MINUTE = os.getenv("ACCES_TOKEN_EXPIRES_IN_MINUTE")
+REFRESH_TOKEN_EXPIRES_IN_DAYS = os.getenv("REFRESH_TOKEN_EXPIRES_IN_DAYS")
+
+password_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 class Directory(BaseModel):
     directory_name: str
+
+class User(BaseModel):
+    username: str
+    password: str
 
 app = FastAPI()
 
@@ -20,6 +36,56 @@ template = Jinja2Templates(directory="templates")
 static_file_directory = StaticFiles(directory="static")
 
 app.mount("/static", static_file_directory, name="static")
+
+users: list[User] = []
+
+@app.get("/login")
+def Login_Page(request: Request):
+
+    return template.TemplateResponse(request, "login.html", {
+
+        "route": "/login"
+    })
+
+@app.post("/login")
+def Login(user_credentials: Annotated[User, Form()]):
+
+    username_input = user_credentials.username
+    password_input = user_credentials.password
+
+    for user in users:
+        if user.username == username_input:
+            print("User Found!")
+            
+            if password_context.verify(password_input, user.password):
+
+                return{
+
+                    "Message":"Authenticated!"
+                }
+            
+            return HTTPException(status_code=401, detail="Invalid Username or Password!")
+    
+    return HTTPException(status_code=401, detail="Invalid Username or Password!")
+
+@app.get("/signup")
+def Signup_Page(request: Request):
+
+    return template.TemplateResponse(request, "signup.html", {
+
+        "route": "/signup"
+    })
+
+@app.post("/signup")
+def Signup(user_credentials: Annotated[User, Form()]):
+
+    user_credentials.password = password_context.hash(user_credentials.password)
+
+    users.append(user_credentials)
+
+    print(users)
+
+    return RedirectResponse("/login", status_code=303)
 
 @app.get("/")
 def Home(request: Request, directory_exist: Optional[bool] = Query(default=False)):
